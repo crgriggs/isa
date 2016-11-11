7d5a719ac326883f4ace2c3a20c7c2035f85ee0f
Michael Brown <mbrown1413@gmail.com>
2016-11-11 13:15:31 -0500

    Make combine.py compatable with python2.4

--- toy_state_model/combine.py
+++ toy_state_model/combine.py
@@ -1,33 +1,28 @@
 #!/usr/bin/env python
 import sys, os
 
+def append_file(f, filename):
+	"""Writes file "filename" to file descriptor f."""
+	f.write("(*----- " + filename + " -----*)\n")
+	f.write(open(filename).read())
+	f.write('\n')
+
 if len(sys.argv) == 2:
 	propType = sys.argv[1]
 else:
 	print "Enter folder that handles current property"
-	quit()
+	sys.exit()
 if not os.path.isdir("./" + str(propType)):
 	print str(propType) + "is not a folder in the current directory" 
-with open("state.ucl", 'w+') as f:
-	with open("header.txt") as h:
-		for line in h:
-			f.write(line)
-		h.close()
-	f.write('\n')
+
+f = open("state.ucl", 'w+')
+try:
+	append_file(f, "header.txt")
 	for filename in os.listdir("./" + str(propType)):
-		with open("./" + str(propType)+"/"+filename) as h:
-			for line in h:
-				f.write(line)
-			h.close()
-		f.write('\n')
-	with open("state.txt") as h:
-		for line in h:
-			f.write(line)
-		h.close()
-	f.write('\n')
-	with open("control.txt") as h:
-		for line in h:
-			f.write(line)
-		h.close()
-
-
+		if filename.endswith(".txt"):
+			append_file(f, "./" + str(propType)+"/"+filename)
+	append_file(f, "state.txt")
+	append_file(f, "control.txt")
+except:
+	f.close()
+	raise
